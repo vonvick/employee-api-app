b4af2ac8a87bbe02063b07d578da869aac5ec215
victornwaiwu <victor.nwaiwu@andela.com>
2019-06-30 14:30:34 +0100

    add models sync to app entry point

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser  from 'body-parser';
 
 import routes from './routes';
+import models from './models';
 
 const app = express();
 const router = express.Router();
@@ -15,6 +16,13 @@ app.use('/api', router);
 
 const port = process.env.PORT || 5600;
 
+models.sequelize.sync().then(() => {
+  console.log('Database server synced');
+}).catch(() => {
+  console.log('Database server could not be synced');
+})
+
 app.listen(port, () => {
+  // Sync sequelize db models
   console.log(`App started on port: ${port}`)
 });
