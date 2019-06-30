eb2558fb31639a4c0a5c976a9475ff81ec6ca046
victornwaiwu <victor.nwaiwu@andela.com>
2019-06-30 14:09:01 +0100

    create model index file

---
+++ src/models/index.js
@@ -0,0 +1,30 @@
+import fs from 'fs';
+import path from 'path';
+import Sequelize from 'sequelize';
+import config from '../config/sequelize.config';
+
+const basename = path.basename(__filename);
+const db = {};
+
+const sequelize = new Sequelize(config.url, config);
+
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+  })
+  .forEach(file => {
+    const model = sequelize['import'](path.join(__dirname, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+export default db;
