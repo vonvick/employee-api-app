bdba521ab33ebeaa96247a8bba61e3ffe0f68602
victornwaiwu <victor.nwaiwu@andela.com>
2019-06-30 13:42:30 +0100

    create entry point for application

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import path from 'path';
+import bodyParser  from 'body-parser';
+
+import routes from './routes';
+
+const app = express();
+const router = express.Router();
+
+routes(router);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/api', router);
+
+const port = process.env.PORT || 5600;
+
+app.listen(port, () => {
+  console.log(`App started on port: ${port}`)
+});
