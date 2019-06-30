56050cfb5ad4359aad31fb9c5cc32340dff2f1d3
victornwaiwu <victor.nwaiwu@andela.com>
2019-06-30 23:41:32 +0100

    add more test for user model file

--- tests/unit/user.test.js
+++ tests/unit/user.test.js
@@ -4,25 +4,58 @@ import factory from '../helpers/user.factory';
 
 const expect = chai.expect;
 
-describe('User Model', () => {
+describe('Employee Model', () => {
   let user;
   let userParams = factory.users;
+
   beforeEach((done) => {
     user = db.Employee.build(userParams);
     done();
   });
 
-  after(async(done) => {
+  after(async() => {
     await db.Employee.sequelize.sync({ force: true });
-    done();
   });
 
-  describe('Create Employee', () => {
+  describe('Create', () => {
     it('creates a Role instance', () => expect(user).to.exist);
 
-    it('has both first and last name', () => {
+    it('has the required user attribute', () => {
       expect(user.first_name).to.equal(userParams.first_name);
       expect(user.last_name).to.equal(userParams.last_name);
+      expect(user.email).to.equal(userParams.email);
+      expect(user.phone).to.equal(userParams.phone);
     });
-  })
+
+    it('saves the user without errors', async () => {
+      const newUser = await user.save();
+
+      expect(newUser.first_name).to.equal(userParams.first_name);
+    });
+  });
+
+  describe('Validations', () => {
+    describe('UNIQUE attributes', () => {
+      it('fails for non unique email', async () => {
+        try {
+          await user.save();
+          const secondUser = await db.User.build(userParams).save()
+  
+          expect(secondUser).to.not.exist
+        } catch (err) {
+          expect(/UniqueConstraintError/.test(err.name)).to.be.true
+        }            
+      });
+    });
+
+    it('fails for invalid email', async () => {
+      try {
+        user.email = 'invalid email';
+        const newUser = await user.save();
+        expect(newUser).to.not.exist;
+      } catch (error) {
+        expect(/Validation isEmail on email failed/.test(error.errors[0].message)).to.be.true;
+      }
+    });
+  });
 });
